d871e7605ed4a17649b8a1ab2dfa90fbad294272
whirling <emma.logsdon2@gmail.com>
2019-11-09 00:05:16 -0500

    base

---
+++ django-vue/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/emmalogsdon/Desktop/personal/myPortfolio/projects/django-vue/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
